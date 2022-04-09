df0772ef29c531f6a559822a13d68df955661adf
duongvandat1998BG <duongdatuneti@gmail.com>
2022-04-09 12:54:20 +0700

    Upload project

---
+++ CodeJavaApp/src/test/java/net/codejava/UserRepositoryTests.java
@@ -0,0 +1,46 @@
+package net.codejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateUser(){
+        User user = new User();
+        user.setEmail("alex@gmail.com");
+        user.setPassword("alex2020");
+        user.setFirstName("Alexander");
+        user.setLastName("Hebb");
+
+        User saveUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, saveUser.getId());
+
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+    }
+
+    @Test
+    public void testFindUserByEmail(){
+        String email = "nam1@codejava.net";
+
+        User user = repo.findByEmail(email);
+
+        assertThat(user).isNotNull();
+
+    }
+}
